4c603b7d7240747390740740e398ab25c27907f8
mikharju <mika.harju@gmail.com>
2026-07-30 08:33:50 +0300

    Remove obsolete guards

--- src/Domain/Systems/CollisionSystem.cs
+++ src/Domain/Systems/CollisionSystem.cs
@@ -107,7 +107,6 @@ public class CollisionSystem : GameSystem
         for (int i = 0; i < _asteroids.Count; i++)
         {
             var (aEntity, aAsteroid) = _asteroids[i];
-            if (!view.HasComponent<Position>(aEntity)) continue;
             foreach (var candidate in _grid.Query(view.GetComponent<Position>(aEntity).Value, aAsteroid.Radius))
             {
                 if (!view.HasComponent<Asteroid>(candidate.Id)) continue;
@@ -126,7 +125,6 @@ public class CollisionSystem : GameSystem
         for (int i = 0; i < _enemyShips.Count; i++)
         {
             var (aEntity, aShip) = _enemyShips[i];
-            if (!view.HasComponent<Position>(aEntity)) continue;
             foreach (var candidate in _grid.Query(view.GetComponent<Position>(aEntity).Value, aShip.Radius))
             {
                 if (!view.HasComponent<EnemyShip>(candidate.Id)) continue;
@@ -356,7 +354,6 @@ public class CollisionSystem : GameSystem
 
         foreach (var (entity, asteroid) in asteroids)
         {
-            if (!view.HasComponent<Position>(entity)) continue;
             var pos = view.GetComponent<Position>(entity);
             _grid.Insert(entity, pos.Value, asteroid.Radius);
         }
