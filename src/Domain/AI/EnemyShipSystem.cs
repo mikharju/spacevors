a023779a1c71e2b406662ab7e15fa3ad42a6d7c9
mikharju <mika.harju@gmail.com>
2026-08-02 23:23:14 +0300

    cancel drift mode for enemies

--- src/Domain/AI/EnemyShipSystem.cs
+++ src/Domain/AI/EnemyShipSystem.cs
@@ -4,6 +4,11 @@ namespace Spacevors.Domain.Systems;
 
 public class EnemyShipSystem : GameSystem
 {
+    private const float DriftSpeedThreshold = 50f;
+    private const float DriftCancelForwardThrust = 18f;
+    private const float DriftCancelSideThrust = 14f;
+    private const float DriftCancelBackThrust = 16f;
+
     public override void Update(WorldView view, float deltaTime, CommandBuffer commands)
     {
         view.GetEntitiesWithComponents<Player, Position>().TryFirst(out var playerTuple);
@@ -13,15 +18,27 @@ public class EnemyShipSystem : GameSystem
         foreach (var (shipEntity, ship, shipPos, currentRot) in view.GetEntitiesWithComponents<EnemyShip, Position, Rotation>())
         {
             if (view.TryGetComponent<Dead>(shipEntity, out _)) continue;
-
             if (!hasPlayer) continue;
 
             var playerPos = playerTuple.Value2;
 
             var toPlayer = playerPos.Value - shipPos.Value;
             float distSq = toPlayer.X * toPlayer.X + toPlayer.Y * toPlayer.Y;
-
             if (distSq > ship.DetectionRange * ship.DetectionRange || distSq < 0.01f) continue;
+
+            view.TryGetComponent<Velocity>(shipEntity, out var vel);
+            var currentVel = vel.Value;
+            float speed = currentVel.Magnitude;
+
+            bool inDriftCancel = speed > DriftSpeedThreshold;
+
+            if (inDriftCancel)
+            {
+                ApplySpinStop(view, shipEntity, ship, deltaTime, commands);
+                ApplyRotationTowardPlayer(view, shipEntity, ship, playerPos.Value, currentRot.Angle, deltaTime, commands);
+                ApplyDriftCancel(view, shipEntity, ship, currentRot.Angle, currentVel, deltaTime, commands);
+                continue;
+            }
 
             view.TryGetComponent<AngularVelocity>(shipEntity, out var angVel);
             float currentAngVel = angVel.Value;
@@ -58,9 +75,6 @@ public class EnemyShipSystem : GameSystem
 
             if (dist <= ship.FiringRange) continue;
 
-            view.TryGetComponent<Velocity>(shipEntity, out var vel);
-            var currentVel = vel.Value;
-
             var targetAccel = toPlayerDir * ship.Acceleration;
             commands.Add(new AddComponentCommand<Acceleration>(shipEntity, new Acceleration(targetAccel)));
 
@@ -76,6 +90,73 @@ public class EnemyShipSystem : GameSystem
                 commands.Add(new AddComponentCommand<Velocity>(shipEntity, new Velocity(newVel)));
             }
         }
+    }
+
+    private static void ApplySpinStop(
+        WorldView view, Entity shipEntity, EnemyShip ship, float deltaTime, CommandBuffer commands)
+    {
+        if (view.TryGetComponent<AngularVelocity>(shipEntity, out var angVel))
+        {
+            float currentAngVel = angVel.Value;
+            if (Math.Abs(currentAngVel) > ship.TurnRate)
+            {
+                float damping = Math.Sign(currentAngVel) * ship.TurnRate * deltaTime;
+                float newAngVel = currentAngVel - damping;
+                if (Math.Sign(newAngVel) != Math.Sign(currentAngVel))
+                    newAngVel = 0f;
+                commands.Add(new AddComponentCommand<AngularVelocity>(shipEntity, new AngularVelocity(newAngVel)));
+            }
+        }
+    }
+
+    private static void ApplyRotationTowardPlayer(
+        WorldView view, Entity shipEntity, EnemyShip ship, Vector2 playerPos,
+        float currentAngle, float deltaTime, CommandBuffer commands)
+    {
+        var toPlayer = playerPos - view.GetComponent<Position>(shipEntity).Value;
+        float distSq = toPlayer.X * toPlayer.X + toPlayer.Y * toPlayer.Y;
+        if (distSq < 0.01f) return;
+
+        float dist = (float)Math.Sqrt(distSq);
+        var toPlayerDir = toPlayer / dist;
+        float targetAngle = (float)Math.Atan2(toPlayerDir.X, -toPlayerDir.Y);
+        float angleDiff = NormalizeAngle(targetAngle - currentAngle);
+
+        if (!float.IsNaN(angleDiff))
+        {
+            float maxTurn = ship.TurnRate * deltaTime;
+            float newAngle = Math.Abs(angleDiff) < maxTurn
+                ? targetAngle
+                : currentAngle + MathF.Sign(angleDiff) * maxTurn;
+            commands.Add(new AddComponentCommand<Rotation>(shipEntity, new Rotation(newAngle)));
+        }
+    }
+
+    private static void ApplyDriftCancel(
+        WorldView view, Entity shipEntity, EnemyShip ship, float facingAngle,
+        Vector2 currentVel, float deltaTime, CommandBuffer commands)
+    {
+        float forwardX = (float)Math.Sin(facingAngle);
+        float forwardY = -(float)Math.Cos(facingAngle);
+        var forwardDir = new Vector2(forwardX, forwardY);
+
+        float sideX = (float)Math.Cos(facingAngle);
+        float sideY = (float)Math.Sin(facingAngle);
+        var sideDir = new Vector2(sideX, sideY);
+
+        float forwardSpeed = Vector2.Dot(currentVel, forwardDir);
+        float sideSpeed = Vector2.Dot(currentVel, sideDir);
+
+        float forwardAccelMag;
+        if (forwardSpeed > 0f)
+            forwardAccelMag = -DriftCancelBackThrust;
+        else
+            forwardAccelMag = DriftCancelForwardThrust;
+
+        float sideAccelMag = -DriftCancelSideThrust * MathF.Sign(sideSpeed);
+
+        Vector2 cancelAccel = forwardDir * forwardAccelMag + sideDir * sideAccelMag;
+        commands.Add(new AddComponentCommand<Acceleration>(shipEntity, new Acceleration(cancelAccel)));
     }
 
     private static float NormalizeAngle(float angle)
